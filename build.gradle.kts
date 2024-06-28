131455310fd61e06aed5b666df43fb9c06c4f203
AnilGudigar <anil.gudigar@gmail.com>
2024-06-28 14:19:22 +0530

    Core and Style kit Module added

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.library") version "8.1.1" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
+    }
+}
+
+allprojects {
+}
+
+tasks{
+    register("clean", Delete::class){
+        delete(rootProject.buildDir)
+    }
+}
