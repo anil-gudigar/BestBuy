0593b52173fd867e1ef08b4b2c0f2727cbb022eb
AnilGudigar <anil.gudigar@gmail.com>
2024-07-07 20:18:28 +0530

    Store locator first version

--- storelocator/build.gradle.kts
+++ storelocator/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     libraries["roomcompiler"]?.let { annotationProcessor(it) }
 
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
